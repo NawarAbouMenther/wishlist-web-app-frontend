4c1f223fa87c3ffb13905fcd126e1307d8bf2474
NawarAbouMenther <nawarabomonter@gmail.com>
2025-11-03 10:50:20 +0100

    Add WishList component with v-for rendering

---
+++ src/components/WishList.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>const wishes = [
+  "Neuer Laptop",
+  "Kopfhörer",
+  "Sneaker",
+  "Gamer Stuhl"
+];
+</script>
+
+<template>
+  <div>
+    <h2>Meine Wishlist</h2>
+    <ul>
+      <li v-for="wish in wishes" :key="wish">{{ wish }}</li>
+    </ul>
+  </div>
+</template>
