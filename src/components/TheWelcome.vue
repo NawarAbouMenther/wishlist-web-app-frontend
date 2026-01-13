0499324ec922f35183e2b15b294b777299a71042
NawarAbouMenther <nawarabomonter@gmail.com>
2026-01-13 16:11:15 +0100

    Restore missing Vue template components for testing

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import IconDocumentation from './icons/IconDocumentation.vue'
+import IconTooling from './icons/IconTooling.vue'
+import IconEcosystem from './icons/IconEcosystem.vue'
+import IconCommunity from './icons/IconCommunity.vue'
+import IconSupport from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <div>
+    <WelcomeItem>
+      <template #icon>
+        <IconDocumentation />
+      </template>
+      <template #heading>Documentation</template>
+      Vue documentation…
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <IconTooling />
+      </template>
+      <template #heading>Tooling</template>
+      Vite tooling…
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <IconEcosystem />
+      </template>
+      <template #heading>Ecosystem</template>
+      Vue ecosystem…
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <IconCommunity />
+      </template>
+      <template #heading>Community</template>
+      Vue community…
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <IconSupport />
+      </template>
+      <template #heading>Support</template>
+      Support Vue…
+    </WelcomeItem>
+  </div>
+</template>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="item">
+    <i><slot name="icon"></slot></i>
+    <div class="details">
+      <h3><slot name="heading" /></h3>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+i {
+  width: 32px;
+  height: 32px;
+}
+</style>
