472f193d4c5e0f21327620da66ae06441a04a78d
NawarAbouMenther <nawarabomonter@gmail.com>
2026-01-13 15:46:07 +0100

    Fix build + logo paths

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ msg?: string }>()
+</script>
+
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-size: 1rem;
+  color: #333;
+}
+</style>
